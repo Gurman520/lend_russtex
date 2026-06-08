3b6dedb89cdc29b4b9e6ca94828f8e7e2c73943d
Romanidzs <romanidzs@gmail.com>
2026-06-08 22:15:16 +0300

    add file

--- public/script.js
+++ public/script.js
@@ -73,37 +73,8 @@ document.addEventListener('DOMContentLoaded', () => {
           if (modalStatus) modalStatus.innerHTML = `<span style="color:#f87171;">Ошибка: ${data.error || 'попробуйте позже'}</span>`;
         }
       } catch (err) {
-        if (modalStatus) modalStatus.innerHTML = '<span style="color:#f87171;">Ошибка сети. Проверьте соединение.</span>';
+        if (modalStatus) modalStatus.innerHTML = '<span style="color:#f87171;">Ошибка сети. Проверьте соединеsние.</span>';
       }
     });
   }
-
-  // ---------- Яндекс.Карта (загрузка API и инициализация) ----------
-//   function initYandexMap() {
-//     const mapContainer = document.getElementById('yandexMap');
-//     if (!mapContainer) return;
-//     // Проверяем, загружен ли ymaps
-//     if (typeof ymaps !== 'undefined') {
-//       ymaps.ready(() => {
-//         const map = new ymaps.Map('yandexMap', {
-//           center: [55.860565, 37.483265],
-//           zoom: 16,
-//           controls: ['zoomControl', 'fullscreenControl']
-//         });
-//         const placemark = new ymaps.Placemark([55.860565, 37.483265], {
-//           balloonContentHeader: 'ООО РусТекс',
-//           balloonContentBody: 'ул. Смольная? 24А'
-//         });
-//         map.geoObjects.add(placemark);
-//       });
-//     } else {
-//       console.warn('Yandex Maps API not loaded');
-//     }
-//   }
-
-//   // Загружаем скрипт API Яндекс.Карт асинхронно
-//   const script = document.createElement('script');
-//   script.src = 'https://api-maps.yandex.ru/2.1/?apikey=ваш_api_ключ&lang=ru_RU';  // Замените на реальный ключ
-//   script.onload = initYandexMap;
-//   document.head.appendChild(script);
-// });
+})
